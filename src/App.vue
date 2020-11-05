93c0a0341cd257764c9d948c2db854a02736b612
Jonathan <j.lacroix98@live.fr>
2020-11-05 11:28:16 +0100

    ddd

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid" style="background-color: brown;">
+    <b-row>
+      <b-col>
+        <Clicker ALIGN="CENTER"/>
+      </b-col>
+      <b-col cols="3">
+        <Units ALIGN="CENTER"/>
+      </b-col>
+      <b-col>
+        <Shop ALIGN="CENTER"/>
+      </b-col>
+    </b-row>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Clicker from './components/Clicker.vue'
+import Units from './components/Units.vue'
+import Shop from './components/Shop.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Clicker,
+    Units,
+    Shop
+  },
+  methods: {
+    increment: function() {
+      this.$store.dispatch("setTotal",this.$store.getters.total+this.$store.getters.parSec);
+    }
+  },
+  mounted: function(){
+    setInterval(this.increment, 1000);
   }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+import Vuex from 'vuex'
+import store from './store'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import VueMoment from 'vue-moment'
+
+Vue.use(BootstrapVue)
+Vue.use(VueMoment)
+Vue.use(store)
+Vue.use(Vuex)
+Vue.use(router)
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
